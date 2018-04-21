829119f90422077e143d8956ea32addc01dd5b9a
Rafael <rafael.ramirez@smartexpose.com>
2018-04-21 20:52:41 +0200

    zwischen speichern

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 
 Route::get('/', 'HomeController@landing');
 
@@ -18,10 +7,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], function() {
 
 //admin Dashboard
-	Route::resource('/', 'DashboardController');
+	Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+
+Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], function() {
 
 //admin 
 //admin Statuses
