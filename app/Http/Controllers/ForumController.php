7fb44705b2ef9fa099c480e5ce12a23db69d6dab
rmunozramirez <rafaelmunoznl@yahoo.com>
2018-04-22 17:18:59 +0200

    zwischen

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -6,11 +6,7 @@ use Illuminate\Http\Request;
 
 class ForumController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         //
